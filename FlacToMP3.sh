92191521132c23149e19b893a680870b26b3846d
Hong Shen Zhou <hong.shenzhou@gmail.com>
2015-08-10 13:40:35 +0800

    Added first round of commenting and documentation.

--- FlacToMP3.sh
+++ FlacToMP3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Variables
-QUALITY=2
-CODEC="a libmp3lame"
-OUTPUTDIR="."
-INPUTTYPE="flac"
-OUTPUTTYPE="mp3"
+QUALITY=2 # Lower is better.
+CODEC="a libmp3lame" # Codec for converting flacs to MP3s.
+OUTPUTDIR="." # Default set as current folder.
+INPUTTYPE="flac" # Input type is flac as default.
+OUTPUTTYPE="mp3" # Output type is MP3 as default.
 
 # Main program function
 function FlacToMP3 () {
@@ -21,12 +21,17 @@ function FlacToMP3 () {
 
   # If input file format and output file format doesn't make sense, then
   else
+
+    # Exits with error value of 1 in case something bad happens.
     echo "Error, $INPUTTYPE and $OUTPUTTYPE mismatch: doesn't make sense!"
     exit 1;
   fi
 }
 
+# Startup message.
 echo "Starting FlacToMP3, version 1.0.0."
 FlacToMP3
+
+# Exits with success as default exit value.
 echo "All done, exiting!"
 exit 0
