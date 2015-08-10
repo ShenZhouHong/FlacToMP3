f0296ab826bbfa8c39da7c9988d1088341d8ab3c
Hong Shen Zhou <hong.shenzhou@gmail.com>
2015-08-10 13:36:10 +0800

    Added proper conditional checking

--- FlacToMP3.sh
+++ FlacToMP3.sh
@@ -2,6 +2,7 @@
 
 # Variables
 QUALITY=2
+CODEC="a libmp3lame"
 OUTPUTDIR="."
 INPUTTYPE="flac"
 OUTPUTTYPE="mp3"
@@ -9,15 +10,15 @@ OUTPUTTYPE="mp3"
 # Main program function
 function FlacToMP3 () {
   # First makes sure that input type and output type are flac and mp3, respectively
-  if [$INPUTTYPE == "flac"] && [$OUTPUTTYPE == "mp3"] 
+  if [ $INPUTTYPE == "flac" ] && [ $OUTPUTTYPE == "mp3" ]
     then
       # Iterates over all .flac files in directory
       for FILE in *.flac
-      do 
+      do
         # Summons ffmpeg command with current file as input, and uses the libmp3lame codec.
-        ffmpeg -i "$FILE" -codec:a libmp3lame -q:a $QUALITY "$OUTPUTDIR/${FILE%.*}.mp3";
+        ffmpeg -i "$FILE" -codec:$CODEC -q:a $QUALITY "$OUTPUTDIR/${FILE%.*}.mp3";
       done
-  
+
   # If input file format and output file format doesn't make sense, then
   else
     echo "Error, $INPUTTYPE and $OUTPUTTYPE mismatch: doesn't make sense!"
